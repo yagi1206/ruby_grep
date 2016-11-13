0d94d3506de1563f71c79f7aa338b2488e8b7b1a
Ryohei Yaginiwa <r.yaginiwa@gmail.com>
2016-11-13 12:45:24 +0900

    confirmed files can be read.

--- test.rb
+++ test.rb
@@ -5,3 +5,24 @@
 ARGV.each do |a|
   puts "arguments is #{a}"
 end
+
+if ARGV.length > 1 then
+  puts "Invalid ARGV number"
+  exit
+end
+
+# Already confirmed that the number of command line argumensts is one
+arg = ARGV[0]
+
+if File.file?("#{arg}") then
+  puts "File exist"
+else
+  puts "File does not exist"
+  exit
+end
+
+
+contents = File.read("#{arg}")
+
+print contents
+
